fa737a058996f9fe00a9cdca726bd26a8f6edb08
isai7710 <isai10sanchez@gmail.com>
2023-11-18 14:44:47 -0700

    Renamed Unit Tests

--- sorter_unittests/sorter_unittests.cpp
+++ sorter_unittests/sorter_unittests.cpp
@@ -9,35 +9,35 @@ class PreBuiltSorter: public testing::Test{
 
 
 // Test Bubble Sort
-TEST_F(PreBuiltSorter, BubbleSortTest) {
+TEST_F(PreBuiltSorter, GivenPreBuiltSorter_WhenCheckingBubbleSort_ExpectValuesInOrder) {
     sorter.BubbleSort();
     std::vector<int> sortedVector = {1, 2, 3, 4, 5, 6, 7, 8};
     EXPECT_EQ(sorter.GetSortedVector(), sortedVector);
 }
 
 // Test Insertion Sort
-TEST_F(PreBuiltSorter, InsertionSortTest) {
+TEST_F(PreBuiltSorter, GivenPreBuiltSorter_WhenCheckingInsertionSort_ExpectValuesInOrder) {
     sorter.InsertionSort();
     std::vector<int> sortedVector = {1, 2, 3, 4, 5, 6, 7, 8};
     EXPECT_EQ(sorter.GetSortedVector(), sortedVector);
 }
 
 // Test Selection Sort
-TEST_F(PreBuiltSorter, SelectionSortTest) {
+TEST_F(PreBuiltSorter, GivenPreBuiltSorter_WhenCheckingSelectionSort_ExpectValuesInOrder) {
     sorter.SelectionSort();
     std::vector<int> sortedVector = {1, 2, 3, 4, 5, 6, 7, 8};
     EXPECT_EQ(sorter.GetSortedVector(), sortedVector);
 }
 
 // Test Merge Sort
-TEST_F(PreBuiltSorter, MergeSortTest) {
+TEST_F(PreBuiltSorter, GivenPreBuiltSorter_WhenCheckingMergeSort_ExpectValuesInOrder) {
     sorter.MergeSort();
     std::vector<int> sortedVector = {1, 2, 3, 4, 5, 6, 7, 8};
     EXPECT_EQ(sorter.GetSortedVector(), sortedVector);
 }
 
 // Test Quick Sort
-TEST_F(PreBuiltSorter, QuickSortTest) {
+TEST_F(PreBuiltSorter, GivenPreBuiltSorter_WhenCheckingQuickSort_ExpectValuesInOrder) {
     sorter.QuickSort();
     std::vector<int> sortedVector = {1, 2, 3, 4, 5, 6, 7, 8};
     EXPECT_EQ(sorter.GetSortedVector(), sortedVector);
